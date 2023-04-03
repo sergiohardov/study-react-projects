09c7b80a62d9e19b1e240b40db244bc2676b5739
Sergio <sergiohardov@gmail.com>
2023-04-03 20:07:34 +0300

    Создан компонент employeers-list

---
+++ employeers/src/components/employeers-list/employeers-list.js
@@ -0,0 +1,15 @@
+import EmployeersListItem from "../employeers-list-item/employeers-list-item";
+
+import "./employeers-list.css";
+
+const EmployeersList = () => {
+  return (
+    <ul className="app-list list-group">
+      <EmployeersListItem />
+      <EmployeersListItem />
+      <EmployeersListItem />
+    </ul>
+  );
+};
+
+export default EmployeersList;
